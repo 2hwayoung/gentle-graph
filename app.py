804d384b0e4fde8ac6fd9a48806a8f2ef2a742f7
seungsu3579 <seungsu3579@gmail.com>
2021-05-19 15:06:49 +0900

    [feat] update

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_cors import CORS
+import json
 
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r'*': {'origins': 'https://d3js.org'}})
 
     @app.route('/')
     def index():
         return render_template('test.html')
         
-    @app.route('/graph')
-    def graph():
-        return render_template('index.html')
+    @app.route('/test')
+    def test():
+
+        data = json.loads("./data/graphFile.json")
+    
+        return jsonify(data)
 
     @app.route('/node')
     def node():
